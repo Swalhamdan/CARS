6f9ff5489fdeed57768921fded4b4e768d5d8518
Swalhamdan <sulaimanalhamdan640@gmail.com>
2023-12-08 22:16:19 +0300

    Added dahboard & working public files

--- routes/account.router.js
+++ routes/account.router.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 
 const accountController = require('../controllers/account.controller');
-console.log (accountController)
 
 // router.post('/register', accountController.postRegister);
 
